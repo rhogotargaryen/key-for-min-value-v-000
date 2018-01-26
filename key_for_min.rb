3b13ffeb7168da1af6f841ff718312a58a6960db
Derek Haller <dhaller.personal@gmail.com>
2018-01-26 06:13:38 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   if name_hash == nil
     return name_hash
   end
-  min_q =
+  min_q = 0
   min_f = ""
   name_hash.collect do |fruit, quant|
     if min_q < quant
